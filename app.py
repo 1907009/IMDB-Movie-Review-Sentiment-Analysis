d7fc84c739f4e04774007ce3f14ec6005d4f6c08
DHaKK <59435551+1907009@users.noreply.github.com>
2021-06-17 12:45:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,url_for,request
+import numpy as np
+import pickle
+import pandas as pd
+import flasgger
+from flasgger import Swagger
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+app=Flask(__name__)
+Swagger(app)
+
+
+mnb = pickle.load(open('C:/Users/Dhakshesh/Documents/Spyder/Naive_Bayes_model_imdb.pkl','rb'))
+countVect = pickle.load(open('C:/Users/Dhakshesh/Documents/Spyder/countVect_imdb.pkl','rb'))
+    
+tfidfVect = pickle.load(open('C:/Users/Dhakshesh/Documents/Spyder/tfidf_imdb.pkl','rb'))
+model = joblib.load('C:/Users/Dhakshesh/Documents/Spyder/tfidfLR.pkl')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+        Reviews = request.form['Reviews']
+        data = [Reviews]
+
+        my_prediction = model.predict(data)
+    return render_template('result.html',prediction = my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,use_reloader=False)
